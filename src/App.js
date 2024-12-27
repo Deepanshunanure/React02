199fd31d5cf46ed96373ee48eec5708e4148a02f
Deepanshunanure <nanuredrwork@gmail.com>
2024-12-27 12:17:50 +0530

    n

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import { useState, useEffect } from 'react';
+import './App.css';
+
+
+
+
+
+const App = () => {
+  
+  return(
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
+  
+
+
+
+export default App;
